a1960afdcf1ab14c306e3295493eac174b9482d8
Esteban Necuse <estebannecuse@gmail.com>
2024-11-27 15:17:42 -0300

    "endpoint get all empelados ok "

--- Back/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ Back/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -1,14 +1,17 @@
 package gm.rh.controlador;
 
+import gm.rh.modelo.Empleado;
 import gm.rh.servicio.EmpleadoServicio;
 import gm.rh.servicio.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 @RestController
@@ -20,5 +23,10 @@ public class EmpleadoControlador {
     @Autowired
     private IEmpleadoServicio empleadoServicio;
 
-    
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados(){
+        List<Empleado> empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado -> logger.info(empleado.toString())));
+        return empleados;
+    }
 }
